75aa6c91aff090dcb501006fd5140465b65a3192
JOAO EMANOEL PAGANI DE OLIVEIRA <1051392421003@fatec.corp>
2026-03-09 11:04:57 -0300

    colocando o sobre

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def casa():
+    return 'Você estar em casa!'
+
+@app.route('/sobre')
+def sobre():
+    return 'Quer saber mais o que'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
