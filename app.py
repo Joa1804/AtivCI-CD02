4f996c1284eed023d4599aa67c1de7ef371eda43
JOAO EMANOEL PAGANI DE OLIVEIRA <1051392421003@fatec.corp>
2026-03-09 11:10:16 -0300

    Eu apreseto os autores

--- app.py
+++ app.py
@@ -10,8 +10,12 @@ def casa():
 def sobre():
     return 'Quer saber mais o que'
 @app.route('/Livros')
-def LIvros():
+def Livros():
     return 'Quer fica esperto ne, aqui teu livro'
+
+@app.route("/Autores")
+def Autores():
+    return "Se você saber esses autores.... sabe muito"
 
 if __name__ == '__main__':
     app.run(debug=True)
